f992fde440cfda54d0824e01afaef1df97d2ee1d
Onionfish2 <123535045+Onionfish2@users.noreply.github.com>
2023-07-30 19:37:57 +0800

    Update subsystem_restart.c

--- drivers/soc/qcom/subsystem_restart.c
+++ drivers/soc/qcom/subsystem_restart.c
@@ -1280,19 +1280,7 @@ int subsystem_restart_dev(struct subsys_device *dev)
 		return 0;
 	}
 
-	switch (dev->restart_level) {
-
-	case RESET_SUBSYS_COUPLED:
-		__subsystem_restart_dev(dev);
-		break;
-	case RESET_SOC:
-		__pm_stay_awake(dev->ssr_wlock);
-		schedule_work(&dev->device_restart_work);
-		return 0;
-	default:
-		panic("subsys-restart: Unknown restart level!\n");
-		break;
-	}
+	__subsystem_restart_dev(dev);
 	module_put(dev->owner);
 	put_device(&dev->dev);
 
